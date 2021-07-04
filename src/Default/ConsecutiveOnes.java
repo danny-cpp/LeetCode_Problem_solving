df16370d87dc0efe796ed2054f592b6f7579abd6
Danh Nguyen <huudanh2908@gmail.com>
2021-07-03 22:10:19 -0400

    ini

---
+++ src/Default/ConsecutiveOnes.java
@@ -0,0 +1,30 @@
+package Default;
+
+public class ConsecutiveOnes {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int res = 0;
+
+        for (int num : nums) {
+            if (num == 1) {
+                res++;
+            } else {
+                if (res > max) {
+                    max = res;
+                }
+                res = 0;
+            }
+        }
+        if (res > max) {
+            max = res;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,1,0,1,1,1};
+
+        ConsecutiveOnes c = new ConsecutiveOnes();
+        System.out.println(c.findMaxConsecutiveOnes(arr));
+    }
+}
