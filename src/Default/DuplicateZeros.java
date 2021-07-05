5d5ee8d7cfda16f9e029b342f79fc77fc112c094
Danh Nguyen <huudanh2908@gmail.com>
2021-07-05 19:12:53 -0400

    Duplicating zeros

---
+++ src/Default/DuplicateZeros.java
@@ -0,0 +1,30 @@
+package Default;
+
+import java.util.Arrays;
+
+public class DuplicateZeros {
+    public void duplicateZeros(int[] arr) {
+        if (arr.length <= 1) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                duplicateZeroAt(arr, i);
+                i++;
+            }
+        }
+    }
+
+    private static void duplicateZeroAt(int[] arr, int position) {
+        if (arr.length - 1 - position >= 0)
+            System.arraycopy(arr, position, arr, position + 1, arr.length - 1 - position);
+        arr[position] = 0;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,0,2,3,0,4,5,0};
+        DuplicateZeros d = new DuplicateZeros();
+        d.duplicateZeros(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
