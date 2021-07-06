da8932bb447e4889d13455ded4f8533344dd13c5
Danh Nguyen <huudanh2908@gmail.com>
2021-07-05 21:58:15 -0400

    Mountain array cp1
    Mountain arr cp 1

---
+++ src/Default/MountainArray.java
@@ -0,0 +1,32 @@
+package Default;
+
+public class MountainArray {
+    public boolean validMountainArray(int[] arr) {
+        if (arr.length < 3) {
+            return false;
+        }
+        if (arr[0] >= arr[1]) {
+            return false;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i+1] == arr[i]) {
+                return false;
+            }
+            else if (arr[i+1] < arr[i]) {
+                for (; i < arr.length - 1; i++) {
+                    if (arr[i+1] >= arr[i]) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,5,5};
+        MountainArray m = new MountainArray();
+        System.out.println(m.validMountainArray(arr));
+    }
+}
