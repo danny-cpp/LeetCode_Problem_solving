4711f2e4d24be26be0a015b33c20c51454b66778
Danh Nguyen <huudanh2908@gmail.com>
2021-07-05 20:36:19 -0400

    merge array

---
+++ src/Default/MergeSortArray.java
@@ -0,0 +1,23 @@
+package Default;
+
+import java.util.Arrays;
+
+public class MergeSortArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if (nums1.length == 0 || nums2.length == 0) {
+            return;
+        }
+        if (n >= 0) System.arraycopy(nums2, 0, nums1, m, n);
+
+        Arrays.sort(nums1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,3,0,0,0};
+        int[] arr2 = {2,5,6};
+        MergeSortArray m = new MergeSortArray();
+        m.merge(arr1, 3, arr2, 3);
+
+        System.out.println(Arrays.toString(arr1));
+    }
+}
