3efe6e120c4c63dd4af7a0afb56b821aade05c15
Danh Nguyen <huudanh2908@gmail.com>
2021-07-06 23:36:51 -0400

    O(n^2) solution

--- src/Default/GreatestElementOnTheRight.java
+++ src/Default/GreatestElementOnTheRight.java
@@ -1,9 +1,30 @@
 package Default;
 
+import java.util.Arrays;
+
 public class GreatestElementOnTheRight {
     public int[] replaceElements(int[] arr) {
+        if (arr.length == 1) {
+            arr[0] = -1;
+            return arr;
+        }
 
+        for (int i = 0; i < arr.length - 1; i++) {
+            int j = i + 1;
+            int tmp_max = arr[j];
+            for (; j < arr.length; j++) {
+                tmp_max = Math.max(tmp_max, arr[j]);
+            }
+            arr[i] = tmp_max;
+        }
+
+        arr[arr.length-1] = -1;
+        return arr;
     }
 
-    
+    public static void main(String[] args) {
+        int[] arr = {17,18,5,4,6,1};
+        GreatestElementOnTheRight g = new GreatestElementOnTheRight();
+        System.out.println(Arrays.toString(g.replaceElements(arr)));
+    }
 }
